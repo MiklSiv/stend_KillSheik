ae9ff36d8bb6424694024289a2de215fd12857cc
Mikl <gnc.rocket@gmail.com>
2023-04-06 17:27:37 +0300

    настройка функции в интерфейсе

--- client_TCP.py
+++ client_TCP.py
@@ -1,6 +1,21 @@
 import socket
 import time
+import random
+import threading
+# переменные для формирования данных клиенту и интерфейсу
 
+stakan_1 = {'DO2V': 1, 'CO2V': 2, 'pHV' : 3, 'DO2N': 4, 'CO2N': 5, 'pHN' : 6, 'oborot' : 7, 'tsr' : 8, 'tvanni' : 9}
+stakan_2 = {'DO2V': 1, 'CO2V': 2, 'pHV' : 3, 'DO2N': 4, 'CO2N': 5, 'pHN' : 6, 'oborot' : 7, 'tsr' : 8, 'tvanni' : 9}
+
+def obnovlenie():
+    while True:
+        for i in stakan_2.keys():
+           stakan_2[i] = random.randint(1, 100)
+        print (stakan_2)
+        time.sleep(1)
+
+der = threading.Thread(target=obnovlenie)
+der.start()
 
 class Client():
     def __init__(self, adress_server, port_server):
@@ -42,6 +57,6 @@ class Client():
 
 
 
-client = Client('127.0.0.1', 5000)
-client.hello_server()
+#client = Client('127.0.0.1', 5000)
+#client.hello_server()
 
